682a64c25a8497cc7af7f42561ad02574546f8b7
castillo.claudia.m.s@gmail.com <castillo.claudia.m.s@gmail.com>
2018-05-31 12:14:01 -0400

    Weather Watch

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Watch</h1>
+		<h3 className="title-container__subtitle">Get current weather conditions for any location.</h3>
+	</div>
+);
+
+export default Titles;
